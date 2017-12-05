48ccc129a839546a5b4b5b531c1b55c12f29a80c
Maciej Królik <maciejkrolik@outlook.com>
2017-12-05 22:40:49 +0100

    Add project files.

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,13 @@
+﻿using AdventOfCode.Days;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day1 day1 = new Day1();
+            day1.PartTwo();
+        }
+    }
+}
